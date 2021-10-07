da30cab863263347065ea0d284976452521b236d
Abdul Khan <akhan@lcs.on.ca>
2021-10-06 20:08:32 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Abdul Ahad Khan on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
